be059b52483f88640450c214dd7e2433068bd504
abdelghani <abdelghaniechlahi514@gmail.com>
2020-11-18 10:46:20 -0100

    commit added

--- js/app.js
+++ js/app.js
@@ -5,4 +5,27 @@ $(document).ready(function() {
         $('header nav').toggleClass('active');
         $(burgger).toggleClass('active');
     });
+
+    // handle the search section
+    let searchLabel = $('#search > label');
+    searchLabel.remove()
+    console.log('search label has removed');
+    // create a fixed header on scroll
+    // grab the hole nav
+    const header = $('header');
+    console.log(header);
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 20) {
+            $(header).animate({
+
+                backgroundColor: 'red',
+                position: 'fixed',
+
+            });
+        } else {
+            $(header).animate({
+
+            });
+        }
+    });
 })
