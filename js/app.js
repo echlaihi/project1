93bd77992f0f7286e6601f405e5ab39987b07077
abdelghani <abdelghaniechlahi514@gmail.com>
2020-11-20 17:36:17 -0100

    carousel script made

--- js/app.js
+++ js/app.js
@@ -28,4 +28,52 @@ $(document).ready(function() {
             });
         }
     });
-})
+
+
+    // carousel script
+    // grab the buttons, the carousel-wrpper, the client's carousel item whith, initialise the couter
+    var carousel = $('.carousel-wrapper');
+    var btnLeft = $('#left');
+    var btnRight = $('#right');
+    var counter = 0;
+
+    // get all the carousel items
+    var numItems = Array.from($('.carousel-item')).length;
+
+    var width = $('.carousel-item').innerWidth();
+
+    // create a slide to left 
+    $(btnLeft).click(function() {
+        if (counter < 1) {
+            return;
+        } else {
+            counter--;
+        }
+
+        // slide to left 
+        $(carousel).css({
+            transform: `translateX(-${counter * width}px)`,
+        });
+
+    });
+
+    // create a slide to right 
+    $(btnRight).click(function() {
+
+        if (counter > numItems - 2) {
+            return
+        } else {
+            counter++;
+        };
+
+        // slide to left 
+        $(carousel).css({
+            transform: `translateX(-${counter * width}px)`,
+        });
+
+    });
+
+
+
+
+});
